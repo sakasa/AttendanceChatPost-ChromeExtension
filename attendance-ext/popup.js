81c37c7aba303c4e7bb750f7d1c3f90ae55a1680
pickles <sakasa@users.noreply.github.com>
2020-08-09 01:47:05 +0900

    Create popup.js
    init

---
+++ attendance-ext/popup.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const webhookElem = document.getElementById('webhook-url');
+chrome.storage.local.get(['webhook'], function(data) {
+  webhookElem.innerText = data.webhook;
+});
+
+const channelElem = document.getElementById('channel');
+chrome.storage.local.get(['channel'], function(data) {
+  channelElem.innerText = data.channel;
+});
+
+const usernameElem = document.getElementById('username');
+chrome.storage.local.get(['username'], function(data) {
+  usernameElem.innerText = data.username;
+});
