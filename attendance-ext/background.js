41061f322662a11f27fabdeccb96f46fc93522df
pickles <sakasa@users.noreply.github.com>
2020-08-08 22:01:20 +0900

    Create background.js
    init

---
+++ attendance-ext/background.js
@@ -0,0 +1,13 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+          conditions: [new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: {
+                hostEquals: 'attendance.moneyforward.com',
+                pathEquals: '/my_page'
+              },
+          })],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
